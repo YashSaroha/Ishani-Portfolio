868e1eb889c79bc5f14795995405d413fc0b9250
unknown <yashsaroha966@gmail.com>
2025-06-28 14:32:19 +0530

    fixed carousel images

--- src/components/Home/SlidingImages.jsx
+++ src/components/Home/SlidingImages.jsx
@@ -3,8 +3,8 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 
 const images = [
-    '/pic1.jpg', '/pic2.jpg', '/pic3.jpg', '/pic4.jpg', '/pic5.jpg',
-    '/pic1.jpg', '/pic2.jpg', '/pic3.jpg', '/pic4.jpg', '/pic5.jpg',
+    'pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg',
+    'pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg',
 ]
 
 export default function Carousel() {
